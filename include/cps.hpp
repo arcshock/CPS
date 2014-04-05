226f42490efbea780a505f5f4bbec74f39e969e8
Bucky Frost <frozenouthere@gmail.com>
2014-04-05 01:54:04 -0800

    Makefile Testing
    Created a simple print function defined in cps.hpp and implemented in
    cps.cpp.
    
    This is simply for testing of the Makefile to ensure that the project is
    compiled.

---
+++ include/cps.hpp
@@ -0,0 +1,17 @@
+/*
+ * Authors: Erik Talvi
+ * 	    Paul Gentemann
+ * 	    Bucky Frost
+ *
+ * File: cps.hpp
+ * Purpose: Simple header file for the testing of the makefile.
+ */
+
+#ifndef CPS_HPP
+#define CPS_HPP
+
+#include <iostream>
+
+void printWorld();
+
+#endif /*CPS_HPP*/
---
+++ src/cps.cpp
@@ -0,0 +1,17 @@
+/*
+ * Authors: Erik Talvi
+ * 	    Paul Gentemann
+ * 	    Bucky Frost
+ *
+ * File: cps.hpp
+ * Purpose: Simple implementation file for the testing of the makefile.
+ */
+
+#include "cps.hpp"
+#include <iostream>
+
+void printWorld()
+{
+	std::cout << "Hello world!" << std::endl;
+}
+
