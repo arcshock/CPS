d287baa01df6611b23a052bb0e54ac06a333cef3
beriukay <beriukay@gmail.com>
2014-04-11 15:06:03 -0800

    smallest possible rectangle that fails first test

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -3,7 +3,17 @@
 
 class Rectangle
 {
+	public:
+		Rectangle(double length, double width) : _length(length), _width(width) 
+		{}
+		double width()
+		{
+			return 0;
+		}
 
+	private:
+		double _length;
+		double _width;
 };
 
 #endif
