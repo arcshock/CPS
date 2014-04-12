7a3bd76050a94a4193cab14d0a935f54a3a969c6
beriukay <beriukay@gmail.com>
2014-04-11 16:34:40 -0800

    Test for two rectangles

--- src/cps.cpp
+++ src/cps.cpp
@@ -1,11 +1,31 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "Rectangle.h"
+#include <string>
+using std::string;
+
+string printSquare = "0.500000 inch 0.500000 inch rmoveto\n"
+					 "-1.000000 inch 0 inch rlineto\n"
+					 "0 inch -1.000000 inch rlineto\n"
+					 "1.000000 inch 0 inch rlineto\n"
+					 "0 inch 1.000000 inch rlineto\n"
+					 "stroke\n";
+string printTestangle = "2.000000 inch 1.500000 inch rmoveto\n"
+					 "-4.000000 inch 0 inch rlineto\n"
+					 "0 inch -3.000000 inch rlineto\n"
+					 "4.000000 inch 0 inch rlineto\n"
+					 "0 inch 3.000000 inch rlineto\n"
+					 "stroke\n";
+
+Rectangle square(1,1);
+Rectangle testangle(3,4);
 
 TEST_CASE( "square"){
-	Rectangle a(1,1);
-	REQUIRE( a.width() == 1);
-	REQUIRE( a.length() == 1);
-	Square square(1);
+	REQUIRE( square.width() == 1);
+	REQUIRE( square.length() == 1);
+	REQUIRE( square.print() == printSquare);
 
+	REQUIRE( testangle.width() == 4);
+	REQUIRE( testangle.length() == 3);
+	REQUIRE( testangle.print() == printTestangle);
 }
