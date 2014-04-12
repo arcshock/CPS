a2dcfa8dc41cd976221d414595a33e74d0ec5f1e
beriukay <beriukay@gmail.com>
2014-04-11 16:34:21 -0800

    generates postscript for arbitrary rectangles

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -1,5 +1,8 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
+#include <string>
+using std::string;
+using std::to_string;
 
 class Rectangle
 {
@@ -13,6 +16,19 @@ class Rectangle
 		double length()
 		{
 			return _length;
+		}
+		string print()
+		{
+			string toPS = to_string(_width / 2.0) + " inch " + to_string(_length / 2.0) +
+					" inch rmoveto\n" + 
+					 "-" + to_string(_width) + " inch 0 inch rlineto\n"
+					 "0 inch -" + to_string(_length) + " inch rlineto\n" +
+					 to_string(_width) + " inch 0 inch rlineto\n"
+					 "0 inch " + to_string(_length) + " inch rlineto\n"
+					 "stroke\n";
+			return toPS;
+
+
 		}
 
 	private:
