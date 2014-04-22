0fd1902f8136489788c4a596e17f558d14fb00dc
eriktalvi <eriktalvi@gmail.com>
2014-04-21 17:35:56 -0800

    colours are pretty

--- testing/filecomp.sh
+++ testing/filecomp.sh
@@ -1,11 +1,13 @@
-cmp ./testing/correctPS.ps ./testing/filecomp.sh
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0m' #No Color
+
+cmp ./testing/correctPS.ps ./testing/testing.ps
     if [ $? -eq 0 ] 
     then 
-		echo -en "\033[1mPS File DOES match\033[0m\n"
+		echo -en "${green}PS File DOES match${NC}\n"
 		tput sgr0
     else 
-		red='\e[0;31m'
-		NC='\e[0m' #No Color
 		echo -en "${red}PS file does NOT match${NC}\n" 
 		tput sgr0
     fi
