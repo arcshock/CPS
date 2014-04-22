48d93b2e0e24e22a3b876cd28ba2a1174222d677
beriukay <beriukay@gmail.com>
2014-04-21 23:23:09 -0800

    moved some subclasses into transformations.h

---
+++ include/Transformations.h
@@ -0,0 +1,65 @@
+
+class Scaled : public Shape
+{
+	public:
+		Scaled(Shape shape, double xScaleFactor, double yScaleFactor)
+		{
+			_height = shape.getHeight() * yScaleFactor;
+			_width = shape.getWidth() * xScaleFactor;
+			_tempPSText = "\t" + toString(xScaleFactor) + 
+				" " + toString(yScaleFactor) + 
+				" scale\n" +
+				shape.getTempPostScriptText();
+		}
+};
+
+class Rotated : public Shape
+{
+	public:
+		Rotated(Shape shape, RotationAngle angle)
+		{
+			setBoundingBox(shape, angle);
+			_tempPSText = "\t" + to_string(angle) +
+							" rotate\n" +
+							shape.getTempPostScriptText();
+		}
+
+		virtual void setBoundingBox(Shape shape, RotationAngle angle)
+		{
+			if (angle == INVERT)
+			{
+				_width = shape.getWidth();
+				_height = shape.getHeight();
+			}
+			_width = shape.getWidth();
+			_height = shape.getHeight();
+		}	
+};
+
+class Colored : public Shape
+{
+	public:
+	Colored(Shape shape, int red, int green, int blue)
+	{
+		_height = shape.getHeight();
+		_width = shape.getWidth();
+
+		string colorizePS = "colorize" + toString(red) + toString(green) + toString(blue);
+		string definePSColor = "/" + colorizePS + " {" + toString(red) + " " + toString(green) +
+								" " + toString(blue) + " setrgbcolor fill} def";
+		
+		_tempPSText = shape.draw();
+		replaceStrokeWithColorize(colorizePS);
+		_tempPSText = definePSColor + "\n\n" + _tempPSText;
+	}
+
+		void replaceStrokeWithColorize(string colorize)
+		{
+			std::size_t foundPoint = _tempPSText.find("stroke"); 
+			while (foundPoint != string::npos)
+			{
+				_tempPSText.replace(foundPoint, 6, colorize);
+				foundPoint = _tempPSText.find("stroke");
+			}
+		}
+};
