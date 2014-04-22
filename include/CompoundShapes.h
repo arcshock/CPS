03d4194bf58350b4c331f452613105c4c6917632
beriukay <beriukay@gmail.com>
2014-04-22 01:37:12 -0800

    flag class basics

--- include/CompoundShapes.h
+++ include/CompoundShapes.h
@@ -8,13 +8,43 @@
 class Flag : public Shape
 {
 	public:
-		Flag() : _height(1), _width(1.9)
+		Flag() 
 		{
-			Star baseStar()
+			_height = 1.0;
+			_width = 1.9;
+			Star baseStar(3.0);
 		}
+
 	protected:
-		double _cantonHeight; 
-		double _cantonWidth;
+		double getCantonHeight()
+		{
+			return _height * 7.0 / 13.0;
+		}
+
+		double getCantonWidth()
+		{
+			return _width * 2.0 / 5.0; 
+		}
+
+		double getStripeWidth()
+		{
+			return _height / 13.0;
+		}
+
+		double getStarDiameter()
+		{
+			return getStripeWidth() * 4.0 / 5.0;
+		}
+		
+		double getSpacerHeight()
+		{
+			return getCantonHeight() * 1.0 / 10.0;
+		}
+
+		double getSpacerWidth()
+		{
+			return getCantonWidth() * 1.0 / 12.0;
+		}
 
 
 };
