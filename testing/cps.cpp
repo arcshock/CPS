f85472432df60063c8b976c3932a511a39185146
eriktalvi <eriktalvi@gmail.com>
2014-04-21 18:55:53 -0800

    refactored out strings from test file

--- testing/cps.cpp
+++ testing/cps.cpp
@@ -32,62 +32,6 @@ string outputShape(string shape)
 	return "gsave\n" + shape;
 }
 
-string printRectangle1 = 
-	"\t0.500000 inch 1.000000 inch rmoveto\n"
-	"\t-1.000000 inch 0 inch rlineto\n"
-	"\t0 inch -2.000000 inch rlineto\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t0 inch 2.000000 inch rlineto\n"
-	"\tstroke\n"
-"grestore\n";
-string printSquare = 
-	"\t1.500000 inch 1.500000 inch rmoveto\n"
-	"\t-3.000000 inch 0 inch rlineto\n"
-	"\t0 inch -3.000000 inch rlineto\n"
-	"\t3.000000 inch 0 inch rlineto\n"
-	"\t0 inch 3.000000 inch rlineto\n"
-	"\tstroke\n"
-"grestore\n";
-string printHexagon =
-	"\t0.500000 inch -0.866025 inch rmoveto\n"
-	"\t60.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t60.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t60.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t60.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t60.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t60.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\tstroke\n"
-"grestore\n";
-string printPentagon = 
-	"\t0.500000 inch -0.769421 inch rmoveto\n"
-	"\t72.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t72.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t72.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t72.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\t72.000000 rotate\n"
-	"\t1.000000 inch 0 inch rlineto\n"
-	"\tstroke\n"
-"grestore\n";
-string printCircle =
-	"\tcurrentpoint 1.000000 inch add moveto\n"
-	"\tcurrentpoint 1.000000 inch sub 1.000000 inch -270 360 arc\n"
-	"\tstroke\n"
-"grestore\n";
-
-
-
-string scaledPrefix = "\t2.000000 2.000000 scale\n";
-string rotatedPrefix = "\t90 rotate\n";
 string listShapes = printSquare + "\n\n" + outputShape(printCircle) + "\n\n" +
 						outputShape(printHexagon) + "\n\n" + outputShape(printPentagon) + "\n";
 
