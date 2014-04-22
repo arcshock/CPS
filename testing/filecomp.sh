6de9da0754173edf2cd0db68aba9484215aae48c
beriukay <beriukay@gmail.com>
2014-04-21 17:29:45 -0800

    red text

--- testing/filecomp.sh
+++ testing/filecomp.sh
@@ -1,9 +1,11 @@
-cmp ./testing/correctPS.ps ./testing/cps.cpp 
+cmp ./testing/correctPS.ps ./testing/filecomp.sh
     if [ $? -eq 0 ] 
     then 
 		echo -en "\033[1mPS File DOES match\033[0m\n"
 		tput sgr0
     else 
-		echo -en "\033[1mPS file does NOT match\033[0m\n" 
+		red='\e[0;31m'
+		NC='\e[0m' #No Color
+		echo -en "${red}PS file does NOT match${NC}\n" 
 		tput sgr0
     fi
