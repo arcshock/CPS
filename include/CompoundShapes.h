018f3ee4b1e64bbe1fcef6743b8219832dc7acfc
beriukay <beriukay@gmail.com>
2014-04-23 18:06:36 -0800

    flag works for size 4 inches... not sure what else

--- include/CompoundShapes.h
+++ include/CompoundShapes.h
@@ -10,17 +10,41 @@
 class Flag : public Shape
 {
 	public:
-		Flag() 
+		Flag(int height) 
 		{
-			_height = 1.0;
-			_width = 1.9;
-			Star baseStar(3.0);
+			_height = 1.0 * height;
+			_width = 1.9 * height * 2;
+
+			Rectangle field(getCantonWidth(), getCantonHeight());
+			Colored fieldOfBlue(field, 0, 0, 1);
+			Spacer topCorner(0, _height * 6.0 / 13.0);
+			Vertical placedField({ topCorner, fieldOfBlue });
+
+			Star starShape(getStarRadius() ); 
+			Scaled fudgeStar(starShape, 1.05, .9);
+			Colored star(fudgeStar, 1, 1, 1);
+			Spacer s(getSpacerWidth() / 2.0 + 1, getSpacerHeight());
+			Spacer h(getSpacerWidth() / 4.0 + 1, getSpacerHeight());
+			Spacer starFieldHeight(0, _height * 6.0 / 13.0 );
+			Spacer bump(4, 5);
+			Horizontal even({bump, h, star, s, star, s, star, s, star, s, star, s, star});
+			Horizontal odd({bump, h, s, star, s, star, s, star, s, star, s, star});
+			Vertical starSpangled({bump, starFieldHeight, even, odd, even, odd, even, odd, even, odd, even});
+
+			Rectangle stripe(getBigStripeWidth(), getStripeHeight());
+			Colored red(stripe, 1, 0, 0);
+			Colored white(stripe, 1, 1, 1);
+			Vertical stripes({red, white, red, white, red, white, 
+					red, white, red, white, red, white, red});
+
+			Layered flag({stripes, placedField, starSpangled});
+			_tempPSText = flag.draw() ;
 		}
 
 	protected:
 		double getCantonHeight()
 		{
-			return _height * 7.0 / 13.0;
+			return _height * 7.0 / 13.0 * .995;
 		}
 
 		double getCantonWidth()
@@ -28,14 +52,24 @@ class Flag : public Shape
 			return _width * 2.0 / 5.0; 
 		}
 
-		double getStripeWidth()
+		double getBigStripeWidth()
+		{
+			return _width;
+		}
+
+		double getSmallStripeWidthScale()
+		{
+			return 3.0 / 5.0;
+		}
+
+		double getStripeHeight()
 		{
 			return _height / 13.0;
 		}
 
-		double getStarDiameter()
+		double getStarRadius()
 		{
-			return getStripeWidth() * 4.0 / 5.0;
+			return .5 * getStripeHeight() * 4.0 / 5.0;
 		}
 		
 		double getSpacerHeight()
@@ -47,8 +81,6 @@ class Flag : public Shape
 		{
 			return getCantonWidth() * 1.0 / 12.0;
 		}
-
-
 };
 
 #endif
